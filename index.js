be25acdc1c35590a0e8bc94dfffd9090c237e8e6
Drake Levy <drakelevy1@gmail.com>
2015-07-09 14:36:54 -0700

    Updated shooting data and GA

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var rawData;
 var raceChart;
 
-googleAnalytics();
-
 function googleAnalytics() {
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -40,6 +38,7 @@ function loadCharts(rawData) {
 	loadStateChart(rawData);
 	loadGenderChart(rawData);
 	loadMentalIllnessChart(rawData);
+	googleAnalytics();
 }
 
 function loadCSVText() {
